9a5b069e74277117228760ad94a3d65b360898bf
MlodziutkiS <sados4f@gmail.com>
2025-02-14 07:36:14 +0100

    3d home

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ const port = 3000
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"))
   })
-
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, "./home.html"))
+  })
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "./about.html"))
+  })
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname, "./form.html"))
+  })
+app.get('/grassTop', (req, res)=>{
+  res.sendFile(path.join(__dirname, "./GrassTop.png"))
+})
+app.get('/grassSide', (req, res)=>{
+  res.sendFile(path.join(__dirname, "./GrassSide.png"))
+})
+app.get('/grassBottom', (req, res)=>{
+  res.sendFile(path.join(__dirname, "./GrassBottom.png"))
+})
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
